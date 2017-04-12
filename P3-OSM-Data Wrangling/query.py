e257e222c43a0fbaa378f1f52bdae765a342d6a0
MashNagesh <mashrajiv@gmail.com>
2017-04-12 19:07:38 +0530

    Update query.py

--- P3-OSM-Data Wrangling/query.py
+++ P3-OSM-Data Wrangling/query.py
@@ -14,34 +14,52 @@ def ways():
     return result.fetchone()[0]
 
 def unique_users():
-    result = cur.execute('SELECT COUNT(DISTINCT(United.UID))             FROM (SELECT UID FROM NODES UNION ALL SELECT UID FROM WAYS)United')
+    result = cur.execute('SELECT COUNT(DISTINCT(United.UID)) FROM (SELECT UID FROM NODES UNION ALL SELECT UID FROM WAYS)United')
     return result.fetchone()[0]
     
 def top_users():
     users = []
-    for row in cur.execute('SELECT United.User,count(*)as num             FROM (SELECT UID,User FROM NODES UNION ALL SELECT UID,User FROM WAYS)United             GROUP BY United.UID             ORDER BY NUM DESC             LIMIT 10'):
+    for row in cur.execute('SELECT United.User,count(*)as num FROM\
+                           (SELECT UID,User FROM NODES UNION ALL SELECT UID,User FROM WAYS)United\
+                           GROUP BY United.UID\
+                           ORDER BY NUM DESC\
+                           LIMIT 10'):
         users.append(row)
     return users
 
 def Avg_contribution():
-    result = cur.execute('SELECT avg(num) FROM            (SELECT United.User as user ,count(*)AS num FROM            (SELECT UID,User FROM NODES UNION ALL SELECT UID,User FROM WAYS)United             GROUP BY United.UID)grouped')
+    result = cur.execute('SELECT avg(num) FROM (SELECT United.User as user,count(*)AS num\
+                         FROM (SELECT UID,User FROM NODES UNION ALL SELECT UID,User FROM WAYS)United\
+                         GROUP BY United.UID)grouped')
     return result.fetchone()[0]
 
 def common_ammenities():
     amenities = []
-    for row in cur.execute('SELECT value,count(*) as num                FROM Node_tags                WHERE key= "amenity"                GROUP BY value                ORDER by num desc                LIMIT 10'):
-                    amenities.append(row)
+    for row in cur.execute('SELECT value,count(*) as num FROM Node_tags \
+                            WHERE key= "amenity" GROUP BY valueORDER by num desc\
+                            LIMIT 10'):
+                                amenities.append(row)
     return amenities
 
 def popular_bank():
     banks =[]
-    for row in cur.execute('SELECT value,count(*)as num FROM Node_tags                         WHERE id in                         (SELECT id FROM Node_tags                         WHERE key="amenity" and (value ="atm" or value ="branch"))and key ="operator"                        GROUP BY value                        ORDER BY num DESC                        LIMIT 10'):
-                            banks.append(row)
+    for row in cur.execute('SELECT value,count(*)as num FROM Node_tags\
+                           WHERE id in (SELECT id FROM Node_tags\
+                           WHERE key="amenity" and (value ="atm" or value ="branch"))and key ="operator"\
+                           GROUP BY value\
+                           ORDER BY num DESC\
+                           LIMIT 10'):
+                                banks.append(row)
     return banks
 
 def popular_cuisines():
     cuisine = []
-    for row in cur.execute('SELECT value,count(*)as num FROM Node_tags                            WHERE id IN                            (SELECT id FROM Node_tags WHERE key="amenity" and value ="restaurant")                           and key ="cuisine"                            GROUP BY value                            ORDER BY num DESC                            LIMIT 5'):
+    for row in cur.execute('SELECT value,count(*)as num FROM Node_tags\
+                        WHERE id IN (SELECT id FROM Node_tags WHERE key="amenity" and value ="restaurant")\
+                        and key ="cuisine" \
+                        GROUP BY value \
+                        ORDER BY num DESC \
+                        LIMIT 5'):
                                 cuisine.append(row)
     return cuisine
 
